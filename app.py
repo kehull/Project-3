86376e982ff32442c83970f6b1574a97737556a1
David Fournie <drfournie@gmail.com>
2021-04-27 15:44:33 -0500

    table

--- app.py
+++ app.py
@@ -94,10 +94,9 @@ def send():
         test={"name":name,"customer_id":customer_id,"gender":gender,"age":age,"income":income,"offer":offer,"membership_date":date}
         #append to table data
         table_data.append(test)
-    else:
-        return
+    
          
-        
+    table_data=[]    
     return render_template("model.html",table_data=table_data)
 
 # @app.route('/model/api')
