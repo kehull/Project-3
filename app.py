b293ad3b23c74b6a88a52c3709ba6d15f36835ce
David Fournie <drfournie@gmail.com>
2021-04-26 23:23:27 -0500

    table test

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 Customer = create_classes(db)
+table_data=[]
 
 @app.route("/")
 def home():
@@ -28,8 +29,9 @@ def about():
 
 @app.route("/model", methods=["GET", "POST"])
 def send():
-    table_data=[]
+    
     if request.method == "POST":
+        table_data=table_data
         name = request.form["name"]
         gender = request.form["gender"]
         age = request.form["age"]
