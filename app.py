5f447e831b176f989984475d5854cc315cb76756
David Fournie <drfournie@gmail.com>
2021-04-27 01:18:12 -0500

    added requirements

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from flask import (
     render_template,
     request,
     redirect)
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
@@ -28,9 +31,10 @@ def about():
     return render_template("about.html")
 
 @app.route("/model", methods=["GET", "POST"])
-table_data=[]
 def send():
-    
+    table_data=[]
+    label_encoder=LabelEncoder()
+    model=joblib.load("models/random_forest_model.h5")   
     if request.method == "POST":
         name = request.form["name"]
         gender = request.form["gender"]
@@ -38,7 +42,12 @@ def send():
         income=request.form["income"]
         customer_id=uuid.uuid4().hex
         date=dt.datetime.today().strftime('%Y%m%d')
-        offer=uuid.uuid4().hex
+        label_encoder.fit(gender)
+        encoded_gender=label_encoder.transform(gender)
+        model_data=[customer_id,encoded_gender,age,income]
+        encoded_predictions = model.predict_classes(model_data)
+        prediction_labels = label_encoder.inverse_transform(encoded_predictions)
+        offer=prediction_labels
         customer = Customer(name=name, customer_id=customer_id, gender=gender, age=age,income=income,offer=offer,membership_date=date)
         db.session.add(customer)
         db.session.commit()
